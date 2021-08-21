7136d05bb4fda58def46abc843d097e78590c992
taba <tabatabaeipour.sa@gmail.com>
2021-08-21 13:13:54 +0430

    complete function of gathering data

--- wtx.py
+++ wtx.py
@@ -1,6 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 
-def 
-url = "https://coinmarketcap.com/currencies/celer-network/"
+def gather ( currency ):
+    url = "https://coinmarketcap.com/currencies/%s/" % currency
+    data = requests.get(url )
+    soup = BeautifulSoup(data.text , 'html.parser')
+    price = soup.find('div' , attrs={"class" : "priceValue___11gHJ"})
+    return price.text.replace("$" , "")
+print(gather("bitcoin"))
+
+
