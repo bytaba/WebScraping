d47042f2aa30095b7d8646d4e068780b47736eee
taba <tabatabaeipour.sa@gmail.com>
2021-08-22 23:03:43 +0430

    now it can read coin names from excel file and fetch the price of coins

--- wtx.py
+++ wtx.py
@@ -1,6 +1,7 @@
+from re import X
 import requests
 from bs4 import BeautifulSoup
-
+from openpyxl import load_workbook
 
 
 def gather ( currency ):
@@ -9,6 +10,27 @@ def gather ( currency ):
     soup = BeautifulSoup(data.text , 'html.parser')
     price = soup.find('div' , attrs={"class" : "priceValue___11gHJ"})
     return price.text.replace("$" , "")
-print(gather("bitcoin"))
+
+def entry ( price , row ):
+    book = load_workbook("../../Ali.xlsx")
+    sheet = book.active
+    sheet.cell(row ,6).value = price
+    book.save("../../Ali.xlsx")
+
+book = load_workbook("../../Ali.xlsx")
+sheet = book.active
+i = 3
+
+while True :
+    name = sheet.cell( i , 2).value
+    if name == None:
+        break
+    print(name)
+    entry(gather(name) , i)
+    i = i + 1
+
+
+
+
 
 
