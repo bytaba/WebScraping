b4fdc00fba5aca3c95225441f4e6edd094c065e3
taba <tabatabaeipour.sa@gmail.com>
2021-08-23 14:52:30 +0430

    reform the structure of codes

--- wtx.py
+++ wtx.py
@@ -8,26 +8,29 @@ def gather ( currency ):
     url = "https://coinmarketcap.com/currencies/%s/" % currency
     data = requests.get(url )
     soup = BeautifulSoup(data.text , 'html.parser')
-    price = soup.find('div' , attrs={"class" : "priceValue___11gHJ"})
+    price = soup.find('div' , attrs={"class" : "priceValue"})
     return price.text.replace("$" , "")
 
-def entry ( price , row ):
-    book = load_workbook("../../Ali.xlsx")
+def entry ( price , row , path):
+    book = load_workbook(path)
     sheet = book.active
     sheet.cell(row ,6).value = price
-    book.save("../../Ali.xlsx")
+    book.save(path)
 
-book = load_workbook("../../Ali.xlsx")
-sheet = book.active
-i = 3
+def read(path):
+    book = load_workbook(path)
+    sheet = book.active
+    i = 3
+    while True :
+        name = sheet.cell( i , 2).value
+        if name == None:
+            break
+        print(name)
+        entry(gather(name) , i , path)
+        i = i + 1
 
-while True :
-    name = sheet.cell( i , 2).value
-    if name == None:
-        break
-    print(name)
-    entry(gather(name) , i)
-    i = i + 1
+path = "../../Ali.xlsx"
+read(path)
 
 
 
